c3da40e39a177322bfad135ed919f7e296794063
Maxim <konstantinovmax@gmail.com>
2021-03-23 17:22:19 +0300

    fix: исправить баг в верстке

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -12,8 +12,8 @@ function AboutMe() {
                     <p className="about-me__profession">Фронтенд-разработчик, 28 лет</p>
                     <p className="about-me__description">Почему я решил стать разработчиком? Во время работы я взаимодействовал с разработчиками в рамках проектной деятельности в роли бизнес и веб-аналитика. Для понимания процесса разработки мне необходимо было обладать техническими знаниями, поэтому я изучал процесс разработки и практиковался сам. В результате меня настолько увлек процесс программирования, что я решил стать веб-разработчиком, чтобы непосредственно самому вести разработку. В веб-разработке я выбрал направление Фронтенд, потому что мне нравится работать с интерфейсом и видеть результат своей работы.</p>
                     <ul className="about-me__links-container">
-                        <li><a href="https://www.facebook.com/konstantinovmax" className="about-me__link" target="_blank" rel="noreferrer">Facebook</a></li>
-                        <li><a href="https://github.com/konstantinovmax" className="about-me__link" target="_blank" rel="noreferrer">Github</a></li>
+                        <li><a href="https://www.facebook.com/konstantinovmax" className="about-me__link about-me__link_type_first" target="_blank" rel="noreferrer">Facebook</a></li>
+                        <li><a href="https://github.com/konstantinovmax" className="about-me__link about-me__link_type_second" target="_blank" rel="noreferrer">Github</a></li>
                     </ul>
                 </article>
                 <img className="about-me__photo" src={studentPhoto} alt="Фото студента" />
--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -9,13 +9,13 @@ function Techs() {
                 <h3 className="techs__title">7 технологий</h3>
                 <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
                 <ul className="techs__list">
-                    <li><p className="techs__item">HTML</p></li>
-                    <li><p className="techs__item">CSS</p></li>
-                    <li><p className="techs__item">JS</p></li>
-                    <li><p className="techs__item">React</p></li>
-                    <li><p className="techs__item">Git</p></li>
-                    <li><p className="techs__item">Express.js</p></li>
-                    <li><p className="techs__item">mongoDB</p></li>
+                    <li className="techs__item-container"><p className="techs__item">HTML</p></li>
+                    <li className="techs__item-container"><p className="techs__item">CSS</p></li>
+                    <li className="techs__item-container"><p className="techs__item">JS</p></li>
+                    <li className="techs__item-container"><p className="techs__item">React</p></li>
+                    <li className="techs__item-container"><p className="techs__item">Git</p></li>
+                    <li className="techs__item-container"><p className="techs__item">Express.js</p></li>
+                    <li className="techs__item-container techs__item-container_type_last"><p className="techs__item">mongoDB</p></li>
                 </ul>
             </div>
         </section>
